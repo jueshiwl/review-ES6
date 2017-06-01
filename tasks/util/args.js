534bb05068a55d8ac6dc1bdc71fbfcee94ae6c69
jueshiwl <469078133@qq.com>
2017-06-01 10:20:42 +0800

    first commit

---
+++ tasks/util/args.js
@@ -0,0 +1,38 @@
+import yargs from 'yargs';
+
+const args = yargs
+
+    .option('production', {
+        boolean: true,
+        default: false,
+        describle: 'min all scripts'
+    })
+
+    .option('watch', {
+        boolean: true,
+        default: false,
+        describle: 'watch all files'
+    })
+
+    .option('verbose', {
+        boolean: true,
+        default: false,
+        describle: 'log'
+    })
+
+    .option('sourcemaps', {
+        boolean: true,
+        default: false,
+        describle: 'force the creation of sroucemaps'
+    })
+
+    .option('port', {
+        string: true,
+        default: 8080,
+        describle: 'sercer port'
+    })
+
+    .argv
+
+export default args;
+    
